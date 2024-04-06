635867d536617c7325b7f6f0d0aaa8e0c87974bc
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-04-06 22:47:00 +0200

    Plugins

--- SemanticKernelPlayground/Program.cs
+++ SemanticKernelPlayground/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
 
 namespace SemanticKernelPlayground;
 
@@ -19,9 +20,21 @@ public class Program
         builder.AddOpenAIChatCompletion(
          "gpt-3.5-turbo",
          openAiKey);
-        
+
+#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        builder.Plugins.AddFromType<TimePlugin>();
+        builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+
         var kernel = builder.Build();
 
+        //await HelloWorld(kernel);
+        //await CurrentDayTimePluginExample(kernel);
+        //await ConversationSummaryPluginExamples(kernel);
+    }
+
+
+    private static async Task HelloWorld(Kernel kernel)
+    {
         var prompt = @"{{$input}}
 
 One line TLDR with the fewest words.";
@@ -33,17 +46,47 @@ One line TLDR with the fewest words.";
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
 3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
 
-        string text2 = @"
-1. An object at rest remains at rest, and an object in motion remains in motion at constant speed and in a straight line unless acted on by an unbalanced force.
-2. The acceleration of an object depends on the mass of the object and the amount of force applied.
-3. Whenever one object exerts a force on another object, the second object exerts an equal and opposite on the first.";
-
         Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text1 }));
+    }
 
-        Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text2 }));
-        
-        Console.WriteLine("Hello, World!");
+    private static async Task CurrentDayTimePluginExample(Kernel kernel)
+    {
+        string prompt = @"Current date {{TimePlugin.Now}} 
+        print me current date in most popular formats";
+
+        var result = await kernel.InvokePromptAsync(prompt);
+        Console.WriteLine(result);
+    }
+
+    private static async Task ConversationSummaryPluginExamples(Kernel kernel)
+    {
+        string input = @"I'm a vegan in search of new recipes. 
+I love spicy food! Can you give me a list of breakfast 
+recipes that are vegan friendly?";
+
+        var summarizeResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "SummarizeConversation",
+            new() { { "input", input } });
+
+        Console.WriteLine(summarizeResult);
+
+        var getTopicsResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "GetConversationTopics",
+            new() { { "input", input } });
+
+        Console.WriteLine(getTopicsResult);
+
+        var actionItemsResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "GetConversationActionItems",
+            new() { { "input", input } });
+
+        Console.WriteLine(actionItemsResult);
     }
 
     private record OpenAi(string ApiKey);
 }
+
+#pragma warning restore SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
