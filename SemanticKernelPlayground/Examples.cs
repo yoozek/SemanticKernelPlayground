b4eb5b2efba7032bc526bcdc019197f9e0506c64
Lukasz Jozwik <ext.lukasz.jozwik@softwareone.com>
2024-04-09 22:31:35 +0200

    Checkpoint

---
+++ SemanticKernelPlayground/Examples.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+namespace SemanticKernelPlayground;
+#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+public class Examples
+{
+    public static async Task HelloWorld(Kernel kernel)
+    {
+        var prompt = @"{{$input}}
+
+One line TLDR with the fewest words.";
+
+        var summarize = kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 100 });
+
+        string text1 = @"
+1st Law of Thermodynamics - Energy cannot be created or destroyed.
+2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
+
+        Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text1 }));
+    }
+
+    public static async Task CurrentDayTimePluginExample(Kernel kernel)
+    {
+        string prompt = @"Current date {{TimePlugin.Now}} 
+        print me current date in most popular formats";
+
+        var result = await kernel.InvokePromptAsync(prompt);
+        Console.WriteLine(result);
+    }
+
+    public static async Task ConversationSummaryPluginExamples(Kernel kernel)
+    {
+        var recipesPrompt = @"User background: 
+            {{ConversationSummaryPlugin.SummarizeConversation $input}}
+            Given this user's background, provide a list of relevant recipes.";
+
+        string input = @"I'm a vegan in search of new recipes. 
+I love spicy food! Can you give me a list of breakfast 
+recipes that are vegan friendly?";
+
+        var suggestRecipes = kernel.CreateFunctionFromPrompt(recipesPrompt);
+        var suggestRecipesResult = await kernel.InvokeAsync(suggestRecipes,
+            new KernelArguments
+            {
+                { "input", input }
+            });
+        Console.WriteLine(suggestRecipesResult);
+
+        var summarizeResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "SummarizeConversation",
+            new() { { "input", input } });
+
+        Console.WriteLine(summarizeResult);
+
+        var getTopicsResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "GetConversationTopics",
+            new() { { "input", input } });
+
+        Console.WriteLine(getTopicsResult);
+
+        var actionItemsResult = await kernel.InvokeAsync(
+            nameof(ConversationSummaryPlugin),
+            "GetConversationActionItems",
+            new() { { "input", input } });
+
+        Console.WriteLine(actionItemsResult);
+    }
+    public static async Task TodoistPluginExamples(Kernel kernel)
+    {
+        var getProjectsPrompt = kernel.CreateFunctionFromPrompt(@"{{TodoistPlugin.GetProjects}}
+for the given list project names in markdown bullet list. Preserve hierarchy
+Projects:");
+        var getProjectsResult = await kernel.InvokeAsync(getProjectsPrompt);
+        Console.WriteLine(getProjectsResult);
+    }
+}
--- SemanticKernelPlayground/Program.cs
+++ SemanticKernelPlayground/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Plugins.Core;
 using SemanticKernelPlayground.Plugins.Todoist;
 
@@ -25,94 +25,55 @@ public class Program
          "gpt-3.5-turbo",
          openAiKey);
 
-#pragma warning disable SKEXP0050 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable SKEXP0050
         builder.Plugins.AddFromType<TimePlugin>();
         builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+#pragma warning restore SKEXP0050
         builder.Plugins.AddFromObject(new TodoistPlugin(todoistApiKey));
 
         var kernel = builder.Build();
 
-        //await HelloWorld(kernel);
-        //await CurrentDayTimePluginExample(kernel);
-        //await ConversationSummaryPluginExamples(kernel);
+        //await Examples.HelloWorld(kernel);
+        //await Examples.CurrentDayTimePluginExample(kernel);
+        //await Examples.ConversationSummaryPluginExamples(kernel);
+        //await Examples.TodoistPluginExamples(kernel);
 
-        await TodoistPluginExamples(kernel);
+        // APL-2005 https://learn.microsoft.com/en-us/training/paths/develop-ai-agents-azure-open-ai-semantic-kernel-sdk/
+
+        //await SuggestChordsPromptExample(kernel);
+
+        var prompts = kernel.ImportPluginFromPromptDirectory("Prompts/TravelPlugins");
+
+        ChatHistory history = [];
+        string input = @"Planuję podróż do Mediolanu z moją narzeczoną. 
+Przylot jest zaplanowany na niedzielę wieczór (do Beregamo), a wylot w środę w południe.
+Lubimy zwiedzać miasto i jeść pyszne jedzenie";
+
+        var result = await kernel.InvokeAsync<string>(prompts["SuggestDestinations"],
+            new() {
+                { "input", input },
+            }
+        );
+
+        Console.WriteLine(result);
+        history.AddUserMessage(input);
+        history.AddAssistantMessage(result);
     }
 
-    private static async Task TodoistPluginExamples(Kernel kernel)
+    private static async Task SuggestChordsPromptExample(Kernel kernel)
     {
-        var getProjectsPrompt = kernel.CreateFunctionFromPrompt(@"{{TodoistPlugin.GetProjects}}
-for the given list project names in markdown bullet list. Preserve hierarchy
-Projects:");
-        var getProjectsResult = await kernel.InvokeAsync(getProjectsPrompt);
-        Console.WriteLine(getProjectsResult);
-    }
+        var prompts = kernel.CreatePluginFromPromptDirectory("Prompts");
+        string input = "G, C";
 
-
-    private static async Task HelloWorld(Kernel kernel)
-    {
-        var prompt = @"{{$input}}
-
-One line TLDR with the fewest words.";
-
-        var summarize = kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 100 });
-
-        string text1 = @"
-1st Law of Thermodynamics - Energy cannot be created or destroyed.
-2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
-3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
-
-        Console.WriteLine(await kernel.InvokeAsync(summarize, new() { ["input"] = text1 }));
-    }
-
-    private static async Task CurrentDayTimePluginExample(Kernel kernel)
-    {
-        string prompt = @"Current date {{TimePlugin.Now}} 
-        print me current date in most popular formats";
-
-        var result = await kernel.InvokePromptAsync(prompt);
+        var result = await kernel.InvokeAsync(
+            prompts["SuggestChords"],
+            new() {
+                { "startingChords", input },
+            }
+        );
         Console.WriteLine(result);
     }
 
-    private static async Task ConversationSummaryPluginExamples(Kernel kernel)
-    {
-        var recipesPrompt = @"User background: 
-            {{ConversationSummaryPlugin.SummarizeConversation $input}}
-            Given this user's background, provide a list of relevant recipes.";
-
-        string input = @"I'm a vegan in search of new recipes. 
-I love spicy food! Can you give me a list of breakfast 
-recipes that are vegan friendly?";
-
-        var suggestRecipes = kernel.CreateFunctionFromPrompt(recipesPrompt);
-        var suggestRecipesResult = await kernel.InvokeAsync(suggestRecipes,
-            new KernelArguments
-            {
-                { "input", input }
-            });
-        Console.WriteLine(suggestRecipesResult);
-
-        var summarizeResult = await kernel.InvokeAsync(
-            nameof(ConversationSummaryPlugin),
-            "SummarizeConversation",
-            new() { { "input", input } });
-
-        Console.WriteLine(summarizeResult);
-
-        var getTopicsResult = await kernel.InvokeAsync(
-            nameof(ConversationSummaryPlugin),
-            "GetConversationTopics",
-            new() { { "input", input } });
-
-        Console.WriteLine(getTopicsResult);
-
-        var actionItemsResult = await kernel.InvokeAsync(
-            nameof(ConversationSummaryPlugin),
-            "GetConversationActionItems",
-            new() { { "input", input } });
-
-        Console.WriteLine(actionItemsResult);
-    }
 
     private record OpenAiConfig(string ApiKey);
 }
